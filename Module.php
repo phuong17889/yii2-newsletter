4c71697a6658306cece5d81871cde7b688c40446
Saurabh Dhariwal <saurabhdhariwal.com@gmail.com>
2016-01-12 17:44:39 +0530

    updated version of file

--- Module.php
+++ Module.php
@@ -5,12 +5,5 @@ use Yii;
 
 class Module extends yii\base\Module
 {
-    public $controllerNamespace = 'saurabhd\newsletter\controllers';
-
-    public function init()
-    {
-        parent::init();
-
-        // custom initialization code goes here
-    }
+    
 }
